160a160552ec9e8d1b1b085ff5c8a8715dda8283
Dmytrii Shypilov <95936002+Dmytrii-Shypilov@users.noreply.github.com>
2022-12-14 00:23:21 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,35 @@
 import os
 import shutil
 import sys
-import pathlib
+from pathlib import Path
 
+files_data = {
+    "audio": {
+        "files": [],
+        "formats": []
+    },
+    "video": {
+        "files": [],
+        "formats": []
+    },
+    "images": {
+        "files": [],
+        "formats": []
+    },
+    "archives": {
+        "files": [],
+        "formats": []
+    }
+}
 
+list = []
+
+folder = sys.argv[1]
+
+path = Path(folder)
+
+for i in path.iterdir():
+    if i.is_file():
+        list.append(i.name)
+
+print(list)
